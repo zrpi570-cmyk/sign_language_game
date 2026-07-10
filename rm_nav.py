30548ab21452af308443e07dd87b9d5ea4c16a02
cxz666 <zrpi570@gmail.com>
2026-07-10 20:47:57 +0800

    Delete rm_nav.py

--- rm_nav.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-for fn in os.listdir('templates'):
-    if not fn.endswith('.html'): continue
-    content = open('templates/'+fn, 'r', encoding='utf-8').read()
-    if link in content:
-        content = content.replace(link+'\\n', '')
-        open('templates/'+fn, 'w', encoding='utf-8').write(content)
-        print(fn+' cleaned')
