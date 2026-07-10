786697e61b366dbbf5ef8335ea05e9a12081794e
cxz666 <zrpi570@gmail.com>
2026-07-10 20:49:37 +0800

    Delete fix_design.py

--- fix_design.py
+++
@@ -1,5 +0,0 @@
-﻿import sys
-sys.stdout.reconfigure(encoding="utf-8")
-c=open("templates/index.html","r",encoding="utf-8").read()
-print("Read OK",len(c))
-c=open("templates/index.html","r",encoding="utf-8").read()
